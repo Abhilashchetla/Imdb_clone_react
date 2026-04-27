e5b2c1019c9d7528f6911763ead3cbf450f5b113
Chetla Abhilash <159163783+Abhilashchetla@users.noreply.github.com>
2026-04-27 18:08:27 +0530

    Update Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Moviecard from "./Moviecard";
 import axios from "axios";
-import Pagination from "./pagination";
+import Pagination from "./Pagination";
 
 function Movies({ handleaddwatlist, handleremwatlist, watchlist }) {
   const [movies, setmovies] = useState([]);
